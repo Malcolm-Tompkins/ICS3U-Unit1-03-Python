48b5cfc0989227472d9f6137bd8e20b8a56390da
Ubuntu <ubuntu@ip-172-31-27-115.ec2.internal>
2021-04-26 18:45:24 +0000

    Added HelloWorld.py, with proper style

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/ python3
+
+# Created by Malcolm Tompkins
+# Created on April 26, 2021
+# A simple "Hello World" file with proper style
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
